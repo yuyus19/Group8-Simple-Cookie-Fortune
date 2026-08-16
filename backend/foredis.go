35542d71a96c39a3258769c09c58e4da5052caeb
jannat731 <97886373+jannat731@users.noreply.github.com>
2026-08-16 20:42:28 +0200

    fixed the things the linter complained about

--- backend/foredis.go
+++ backend/foredis.go
@@ -118,8 +118,8 @@ func init() {
 		if err != nil {
 			fmt.Println("redis hget failed", err.Error())
 		} else {
-			idx := fmt.Sprintf("%s", key.([]byte))
-			msg := fmt.Sprintf("%s", val.([]byte))
+			idx := string(key.([]byte))
+			msg := string(val.([]byte))
 			datastoreDefault.m[idx] = fortune{ID: idx, Message: msg}
 			fmt.Printf("%s => %s\n", key, val)
 		}
--- backend/main.go
+++ backend/main.go
@@ -142,7 +142,7 @@ func (h *fortuneHandler) Get(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("redis hget failed", err.Error())
 		} else {
 			if val != nil {
-				msg := fmt.Sprintf("%s", val.([]byte))
+				msg := string(val.([]byte))
 				h.store.Lock()
 				h.store.m[key] = fortune{ID: key, Message: msg}
 				h.store.Unlock()
